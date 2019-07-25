aae870472b960e4dcab6f62d55829be2a0048836
Danny Suarez <dsuarezlogans@gmail.com>
2019-07-25 18:49:41 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.scss';
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          { /* eslint-disable-next-line */ }
+          Soon a cv site made with ❤️ with React. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

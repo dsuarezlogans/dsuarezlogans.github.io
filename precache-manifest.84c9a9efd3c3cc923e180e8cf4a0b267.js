68a80c1eb03d4b17726dd16b8babfe720531e5ef
Danny Suarez <dsuarezlogans@gmail.com>
2019-07-25 19:10:15 -0500

    add start react app for cv page

---
+++ precache-manifest.84c9a9efd3c3cc923e180e8cf4a0b267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550566f83f117fed183af53bf1dd781b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45c72b7c1979a896a465",
+    "url": "/static/css/main.ee0137fc.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "45c72b7c1979a896a465",
+    "url": "/static/js/main.82d733fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
